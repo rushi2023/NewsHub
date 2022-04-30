dac4614c86bb514d731583fdc9bda2a6e7ab60b8
rushi2022 <191200107018@asoit.edu.in>
2022-04-30 23:19:46 +0530

    main

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <HashRouter >
+  <BrowserRouter basename="/NewsHub" >
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
